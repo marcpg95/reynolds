abc7e5b050f214573b8e3bada5fcb8366ea240f9
Admin <Admin@BCN-6FGGSG2.usersad.everis.int>
2019-11-19 13:04:28 +0100

    arreglos

--- src/funciones/Cobrar.java
+++ src/funciones/Cobrar.java
@@ -10,46 +10,20 @@ import javax.swing.JInternalFrame;
 import javax.swing.JTable;
 
 public class Cobrar {
-	//* Mueve el fichero de comandas a facturas
-	public void CobrarBarra(int numeroMesa){
-		
-		
-			
-			
-		 File borrarComanda = new File("Comandas/comanda" + (numeroMesa) + ".xml");
-		 borrarComanda.renameTo(new File("Facturas/comanda" + (numeroMesa) + ".xml"));
-		 
-		
-	     
-		 
-	        
+	// * Mueve el fichero de comandas a facturas
+	public void CobrarBarra(int numeroMesa) {
 
-		
-		
-		
-		
-		
+		File borrarComanda = new File("Comandas/comanda" + (numeroMesa) + ".xml");
+		borrarComanda.renameTo(new File("Facturas/comanda" + (numeroMesa) + ".xml"));
+
 	}
-	//Recupera el fichero de Facturas a comandas
-	public void RecuperarCobrarBarra(int numeroMesa){
-		
-		
-			
-		
-		 File borrarComanda = new File("Facturas/comanda" + (numeroMesa) + ".xml");
-		 borrarComanda.renameTo(new File("Comandas/comanda" + (numeroMesa) + ".xml"));
-		
-		 
-	        
 
-		
-		
-		
-		
-		
+	// Recupera el fichero de Facturas a comandas
+	public void RecuperarCobrarBarra(int numeroMesa) {
+
+		File borrarComanda = new File("Facturas/comanda" + (numeroMesa) + ".xml");
+		borrarComanda.renameTo(new File("Comandas/comanda" + (numeroMesa) + ".xml"));
+
 	}
-	
 
-
-   
 }
