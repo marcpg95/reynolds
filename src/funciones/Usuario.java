4aa5e6df743054ae8b65afd48821fe1f746210af
SergioPujol98 <sergiopujol.98@gmail.com>
2019-12-04 16:16:09 +0100

    Haciendo InternalFrame Password

--- src/funciones/Usuario.java
+++ src/funciones/Usuario.java
@@ -1,6 +1,7 @@
 package funciones;
 
 import java.awt.BorderLayout;
+
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
@@ -16,6 +17,8 @@ import java.util.Map.Entry;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JDesktopPane;
+import javax.swing.JFrame;
 import javax.swing.JInternalFrame;
 import javax.swing.JLabel;
 import javax.swing.JMenu;
@@ -23,6 +26,7 @@ import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
+import javax.swing.WindowConstants;
 import javax.swing.event.DocumentEvent;
 import javax.swing.event.DocumentListener;
 import javax.swing.plaf.basic.BasicInternalFrameUI;
@@ -32,6 +36,7 @@ import org.apache.commons.codec.digest.DigestUtils;
 import com.mms.mms.clases.Camarero;
 import com.mms.mms.clases.Categories;
 
+
 import baseDeDatos.ConsultarCamareros;
 import interfaz.Principal;
 
@@ -39,66 +44,152 @@ public class Usuario {
 	static JButton botonCamarero = new JButton();
 
 	public static void InputDialog(JMenuItem menuCocina, JMenuItem menuBarra, JButton Servir, JButton Devolver,
-			JInternalFrame barra, JMenu mnBarraCocina, String nombreCamarero,JInternalFrame internalLogin) {
+			JInternalFrame barra, JMenu mnBarraCocina, String nombreCamarero,JInternalFrame internalLogin, JPanel internalFrames) {
 
 		String password = Principal.camareros.get(nombreCamarero).getPassword(); //COJO LA PASSWORD DEL CAMARERO
 		
-		JTextField t1 = new JTextField();
-		JPanel teclado = new JPanel();
-		teclado.setLayout(new GridBagLayout());
-		GridBagConstraints c = new GridBagConstraints();
-		c.weightx = 0;
-		c.weighty = 0;
-		c.fill = GridBagConstraints.HORIZONTAL;
-		String row0 = "1234567890qwertyuiopasdfghjklñzxcvbnm,.-";
-		int contadorx = 0;
-		int contadory = 1;
-		for (int i = 0; i < row0.length(); i++) {
-			char[] key0 = row0.toCharArray();
-			JButton button = new JButton(Character.toString(key0[i]));
-			String tecla = Character.toString(key0[i]);
+		
+		
+		//boolean correcto = false;
+		
+			//while (!correcto) {
+				Font negrita = new Font("Arial", Font.BOLD, 14);
+				
+				internalLogin.setVisible(true);
+				
+				JInternalFrame internalPassw = new JInternalFrame("Login");
+				internalPassw.setBounds(250, 150, 400, 450);
+				internalFrames.add(internalPassw);
 
-			button.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent e) {
+				
+				// Se construye el panel que ira dentro del JInternalFrame
+				
+				JPanel panelPassw = new JPanel();
+				panelPassw.setBounds(68, 5, 252, 246);
+				panelPassw.setLayout(new GridLayout(3, 5, 5, 10));
+				internalPassw.setVisible(true);
+				
+				JPanel teclado = new JPanel();
+				teclado.setLayout(new GridBagLayout());
+				teclado.setBounds(-5, 20, 400, 300);
 
-					String datos = t1.getText();
-					t1.setText(datos + tecla);
+				// Se ponen los panels dentro del internal
+				internalPassw.add(teclado, BorderLayout.SOUTH);
+				internalPassw.add(panelPassw);
 
-					JButton obj = (JButton) e.getSource();
-					String boton = obj.getText();
-					if (boton.contentEquals("-")) {
+				GridBagConstraints gbConstraints = new GridBagConstraints();
+				gbConstraints.weightx = 0;
+				gbConstraints.weighty = 0;
+				gbConstraints.fill = GridBagConstraints.HORIZONTAL;
+
+				
+				JLabel labelPassword = new JLabel("Contraseña");
+				labelPassword.setFont(negrita);
+				panelPassw.add(labelPassword);
+				JTextField t1 = new JTextField(10);
+				panelPassw.add(t1);
+				
+				JButton botonLogin = new JButton("Login");
+
+				botonLogin.addActionListener(new ActionListener() {
+					public void actionPerformed(ActionEvent e) {
+						String panePassword = t1.getText();
+						panePassword = DigestUtils.sha1Hex(panePassword); //ENCRIPTO LA PASSWORD
+						if (panePassword.equals(password)) { //COMPRUEBO SI SON LA MISMA
+							JOptionPane.showMessageDialog(null, "Bienvenido " + nombreCamarero + "!");
+							menuBarra.setEnabled(true);
+							Devolver.setEnabled(true);
+							menuCocina.setEnabled(true);
+							Servir.setEnabled(true);
+							barra.setVisible(true);
+							mnBarraCocina.setEnabled(true);
+							//correcto = true;
+							internalLogin.setVisible(false);
+						} else {
+							JOptionPane.showMessageDialog(null, "Contraseña incorrecta vuelve a escribirla");
+						}
+						
+					}
+				});
+				panelPassw.add(botonLogin);
+
+				
+				// Creamos el string que usaremos para el teclado.
+				String row0 = "1234567890qwertyuiopasdfghjklñzxcvbnm,.-";
+				int contadorx = 0;
+				int contadory = 1;
+				for (int i = 0; i < row0.length(); i++) {
+					char[] key0 = row0.toCharArray();
+					JButton button = new JButton(Character.toString(key0[i]));
+					String tecla = Character.toString(key0[i]);
+
+					button.addActionListener(new ActionListener() {
+						public void actionPerformed(ActionEvent e) {
+
+							String datos = t1.getText();
+							t1.setText(datos + tecla);
+
+							JButton teclas = (JButton) e.getSource();
+							String boton = teclas.getText();
+							if (boton.contentEquals("-")) {
+							}
+						}
+					});
+					gbConstraints.gridx = contadorx;
+					gbConstraints.gridy = contadory;
+					gbConstraints.gridwidth = 1;
+					teclado.add(button,gbConstraints);
+					contadorx++;
+					if (contadorx == 10) {
+						contadory++;
+						contadorx = 0;
 					}
 				}
-			});
-			c.gridx = contadorx;
-			c.gridy = contadory;
-			c.gridwidth = 1;
-			teclado.add(button, c);
-			contadorx++;
-			if (contadorx == 10) {
-				contadory++;
-				contadorx = 0;
-			}
-		}
 
-		JButton espacio = new JButton(" ");
-		espacio.addActionListener(new ActionListener() {
-			@Override
-			public void actionPerformed(ActionEvent e) {
-				String datos = t1.getText();
-				t1.setText(datos + " ");
-			}
-		});
-		c.gridx = 3;
-		c.gridy = 5;
-		c.gridwidth = 4;
-		teclado.add(espacio, c);
-		
-		boolean correcto = false;
-		String panePassword;
-			while (!correcto) {
-				panePassword = (String) JOptionPane.showInputDialog("Escribe la contraseña");
+				// Boton de espacio
+				JButton espacio = new JButton(" ");
+				espacio.addActionListener(new ActionListener() {
+					@Override
+					public void actionPerformed(ActionEvent e) {
+						String datos = t1.getText();
+						t1.setText(datos + " ");
+					}
+				});
+				gbConstraints.gridx = 3;
+				gbConstraints.gridy = 5;
+				gbConstraints.gridwidth = 4;
+				teclado.add(espacio, gbConstraints);
+
+				// Con esto quitamos la flecha desplegable superior.
+				BasicInternalFrameUI ui = (BasicInternalFrameUI) internalPassw.getUI();
+				Container north = (Container) ui.getNorthPane();
+				north.remove(0);
+				north.validate(); // Usamos validate y repaint debido a que el contenedor ya era visible, por lo
+									// tanto tienes que llamarlos para validar el cambio.
+				north.repaint();
+
+				internalPassw.setClosable(false);// Para que se pueda cerrar.
+				internalPassw.setResizable(false); // Para que se pueda redimensionar el internalFrame.
+				// internalLogin.getContentPane().setLayout(null); //Para modificar la posicion
+				// de los componentes internos
+				internalFrames.add(internalPassw);
+
+				// Se visualiza el JInternalFrame
+				internalPassw.setVisible(true);
 				
+				
+				
+
+		
+		
+		
+				
+				
+				
+				
+				
+				//panePassword = "";//(String) JOptionPane.showInputDialog(null,teclado,"Escribe la contraseña");
+				/*
 				
 				panePassword = DigestUtils.sha1Hex(panePassword); //ENCRIPTO LA PASSWORD
 				if (panePassword.equals(password)) { //COMPRUEBO SI SON LA MISMA
@@ -114,7 +205,9 @@ public class Usuario {
 				} else {
 					JOptionPane.showMessageDialog(null, "Contraseña incorrecta vuelve a escribirla");
 				}
+				
 		} 		
+		*/
 	}
 
 	public static void LoginSinAyuda(JMenuItem menuCocina, JMenuItem menuBarra, JButton Servir, JButton Devolver,
@@ -171,7 +264,7 @@ public class Usuario {
 					final String nombreCamareroLeido = leerCamarero.getNombre();
 					btnCamarero.addActionListener(new ActionListener() {
 						public void actionPerformed(ActionEvent e) {
-							Usuario.InputDialog(menuCocina, menuBarra,Servir,Devolver,barra, mnBarraCocina, nombreCamareroLeido,internalLogin);							
+							Usuario.InputDialog(menuCocina, menuBarra,Servir,Devolver,barra, mnBarraCocina, nombreCamareroLeido,internalLogin, internalFrames);							
 						}
 
 					});
